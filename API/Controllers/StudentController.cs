f8615c8ef09ba9d9daf70a2a112acc204065a46f
Umair Ali <u.ali.digitscommerece@gmail.com>
2025-02-06 10:31:56 +0500

    Fist Push

---
+++ API/Controllers/StudentController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : Controller
+    {
+        static List<Students> std = new List<Students>();
+
+        // GET: Student
+        [HttpGet]
+
+        public IEnumerable<Students> Get()
+        {
+            return std;
+        }
+
+        [HttpGet("id")]
+        public Students Get(int id)
+        {
+            return std.FirstOrDefault(s => s.Id == id);
+        }
+
+
+        [HttpPost]
+        public void Post([FromBody] Students value)
+        {
+            std.Add(value);
+        }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Students value)
+        {
+            int i = std.FindIndex(s => s.Id == id);
+            if(i>=0)
+                std[i]= value; 
+        }
+        [HttpDelete("id")]
+        public void Delete (int id)
+        {
+            std.RemoveAll(std => std.Id == id);
+        }
+
+
+    }
+}
